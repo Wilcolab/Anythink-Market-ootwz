286981fe4493a1a6b40c942cdc6e9c9f6e1b1779
On Freund <on@wilcohq.com>
2024-04-08 13:47:21 +0000

    Add GET and DELETE routes for comments

--- backend/routes/api/comments.js
+++ backend/routes/api/comments.js
@@ -3,3 +3,33 @@ const mongoose = require("mongoose");
 const Comment = mongoose.model("Comment");
 
 module.exports = router;
+
+router.get("/", (req, res) => {
+    Comment.find()
+        .then((comments) => {
+        res.json(comments);
+        })
+        .catch((err) => {
+        console.log(err);
+        });
+    }
+);
+
+// Implement a delete endpoint for comments
+router.delete("/:id", async (req, res) => {
+    try {
+        /**
+         * Represents a comment object.
+         * @typedef {Object} Comment
+         * @property {string} id - The unique identifier of the comment.
+         * @property {string} content - The content of the comment.
+         * @property {string} author - The author of the comment.
+         * @property {Date} createdAt - The date and time when the comment was created.
+         * @property {Date} updatedAt - The date and time when the comment was last updated.
+         */
+        const comment = await Comment.findByIdAndRemove(req.params.id);
+        res.json(comment);
+    } catch (err) {
+        console.log(err);
+    }
+});
