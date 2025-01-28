59b58fbe97a906c02bb56a958c6d3efe5a8d5200
On Freund <on@wilcohq.com>
2025-01-28 16:41:47 +0000

    Add GET and DELETE routes for comments with error handling

--- backend/routes/api/comments.js
+++ backend/routes/api/comments.js
@@ -1,5 +1,50 @@
+/**
+ * Express router providing comment related routes.
+ * @module routes/api/comments
+ */
+
 const router = require("express").Router();
 const mongoose = require("mongoose");
 const Comment = mongoose.model("Comment");
 
+/**
+ * Get all comments.
+ * @name GET /
+ * @function
+ * @memberof module:routes/api/comments
+ * @inner
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ * @returns {JSON} - A JSON array of all comments
+ */
+
+/**
+ * Delete a comment by ID.
+ * @name DELETE /:id
+ * @function
+ * @memberof module:routes/api/comments
+ * @inner
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ * @returns {StatusCode} - 204 No Content on successful deletion
+ */
+
 module.exports = router;
+
+router.get("/", async (req, res) => {
+    try {
+        const comments = await Comment.find();
+        res.json(comments);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+router.delete("/:id", async (req, res) => {
+    try {
+        await Comment.findByIdAndRemove(req.params.id);
+        res.sendStatus(204);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
